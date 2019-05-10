3b159530ac26dc1876a711c2a42e5ac5b0ff63a0
fep <franck.en.planck@gmail.com>
2019-05-10 13:53:02 +0200

    Commentaires - fin RechercheMode1 - RechercheMode2

--- src/packageProjetMasterMind/Main.java
+++ src/packageProjetMasterMind/Main.java
@@ -17,13 +17,11 @@ public class Main {
 	static int CombinaisonAttaque [];
 	
 	static int reponse1 [];
-	
-	
+
+	Scanner sc = new Scanner(System.in);
 	
 	
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
 		
 // 1 Choix du jeu et du mode
 		
@@ -39,135 +37,16 @@ public class Main {
 
 		CombinaisonSecreteOrdi cso = new CombinaisonSecreteOrdi();
 		combinaisonDefense= cso.getCombinaisonDefense();
-		// int getCombinaisonAttaque [] = csoej.getCombinaisonAttaque();
-		// reponse1= csoej.getReponse1();
-
+		
 	// 2 Recherche +/- : 
 	
-	// 2.1 Recherche +/- Mode 1 : 'Challenger' o� vous devez trouver la combinaison secréte de l'ordinateur
-	if (modeJeu == 1) {
-		String verdict = new String();
-		// Variable réponse corrective de l'ordinateur
-		String correctif []= new String[nPions];
-		int tour = 0;
-		int tourRestant = nCoups;
-		boolean boleenSiGagne;
-		
-		//2.1.1 par tableau (combinaison1, reponse1)
-		while (verdict != "GAGNE!" || verdict != "PERDU!") {
-			
-			//2.1.1.1 entrées du joueur (var reponse1)
-			tour++;
-			System.out.println("\n \n Veuillez entrer votre code:");
-			
-// Ctest de l'entrée
-			
-			TestEntreesJeu entreesJeu = new TestEntreesJeu (reponse1,nCouleurs);
-			
-			//2.1.1.2 comparatif combiaison secrète / entrée => réponse ordinateur
-			boleenSiGagne = true;
-			
-			for (int i=0; i<nPions; i++) {
-				if ( reponse1[i]==combinaisonDefense[i] ) {
-					correctif[i]="=";
-				}
-				if ( reponse1[i]<combinaisonDefense[i] ) {
-					correctif[i]="-";
-					boleenSiGagne = false;
-				}
-				if ( reponse1[i]>combinaisonDefense[i] ) {
-					correctif[i]="+";
-					boleenSiGagne = false;
-				}
-			}
-			//2.1.1.3 Affichage & tests
-			System.out.println("Réponse:");
-			for (int i=0; i<nPions; i++) {
-				System.out.print(correctif[i]);
-			}
-			
-			//2.1.1.4 test
-			tourRestant = nCoups - tour;
-			System.out.println(" -> Il vous reste: "+ tourRestant + " coups!");
-			System.out.println(" TRICHE 2 : reponse1 "+ reponse1 + " 1 combinaisonDefense "+ combinaisonDefense);
-			if (tourRestant == 0 &&  !boleenSiGagne ) {
-				verdict = "PERDU!";
-			}
-			else if (boleenSiGagne) {
-				verdict = "GAGNE!";
-			}
-		}
-		System.out.println(verdict);
-	}
+	// 2.1 Recherche +/- Mode 1 : 'Challenger' où vous devez trouver la combinaison secréte de l'ordinateur
+	
+		RechercheMode1 rm1 = new RechercheMode1();
 			
 			// 2.2 Recherche +/- : mode 2 : A l'ordinateur de trouver votre combinaison secrète
-	if (  modeJeu == 2) { 
-		String verdict = new String();
-		verdict = "";
-		// Nombre de tours
-		int tour = 0;
-		// Création tableau 2d réponse (/joueur) j->position pion, k->N coup.
-		String tableauReponseJoueur [][]=new String [nPions][nCoups];
-		// Création tableau 3d (/ordi)  i->position pion, j->N coup.
-		int tableauJeu [][] = new int [nPions][nCoups];	
-		
-		
-	// 2.2.1 Entr�es du joueur (var combinaisonDefense)
-		for (int i = 0 ; i < nPions; i++) {
-			System.out.println("veuillez entrer la couleur du " + (i+1) + " eme pion: ");
-			combinaisonDefense [i]= sc.nextInt();
-		}
-		System.out.print("\n ---------------------"+"\nLa proposition est donc: ");
-		for (int i = 0 ; i < nPions; i++) {
-			System.out.print(combinaisonDefense [i]);
-		}
-		System.out.print("\n ---------------------");
-		
-	// 2.2.2 Recherche et affichage de l'ordi.	
-	
-		// Recherche par l'ordi.
-		while (verdict != "GAGNE!" || verdict != "PERDU!") {
-			for (int j=0;j<nCoups;j++) {
-				tour++;
-				System.out.println("Nous sommes au tour :" + tour);
-				// 1er coup: On donne une couleur aléatoire differente à chaque pion
-				if (tour==1) {
-					for (int i=0;i<nPions;i++) {
-					tableauJeu[i][0]=(int)(Math.random()*nCouleurs);
-					System.out.println("Pion "+ i + " : " + tableauJeu[i][0]);
-					}
-				// Sinon, l'ordi. tient compte de ce qu'a dit le joueur au coup d'avant
-			} else {
-				for (int i=0;i<nPions;i++) {	
-					if (tableauReponseJoueur[i][j-1]=="=") {
-						tableauJeu[i][j]=tableauJeu[i][j-1];
-						System.out.println("Pion "+ i + " : " + tableauJeu[i][j]);
-						continue;
-						}			
-					if (tableauReponseJoueur[i][j-1]=="+") {
-						tableauJeu[i][j]=(tableauJeu[i][j-1]+nCouleurs)/2;
-						System.out.println("Pion "+ i + " : " + tableauJeu[i][j]);
-						continue;
-						}			
-					if (tableauReponseJoueur[i][j-1]=="-") {
-						tableauJeu[i][j]=(tableauJeu[i][j-1])/2;
-						System.out.println("Pion "+ i + " : " + tableauJeu[i][j]);
-						}
-					}
-				}
-				// 2.2.3 Entrée de la réponse du joueur (var tableauReponseJoueur)
-				for (int i=0;i<nPions;i++) {
-				System.out.println("Veuillez entrer la r�ponse du " + (i+1) + " eme pion.");
-				tableauReponseJoueur[i][j]=sc.nextLine();
-				while (tableauReponseJoueur[i][j] != "=" && tableauReponseJoueur[i][j] != "+" 
-						&& tableauReponseJoueur[i][j] != "-") {
-					System.out.println("Merci d'entrer une valeur ad�quate: + ou - ou =");
-					tableauReponseJoueur[i][j]=sc.nextLine();
-					}
-				}
-			}
-		}	
-	}
+
+		RechercheMode2 rm2 = new RechercheMode2();
 			
 	
 			// 2.3 Recherche +/- : mode 3
---
+++ src/packageProjetMasterMind/RechercheMode1.java
@@ -0,0 +1,65 @@
+package packageProjetMasterMind;
+
+public class RechercheMode1 {
+	
+// Renvoie si la partie est gagnée ou non
+String verdict = new String();
+// Variable réponse corrective de l'ordinateur en fonction du jeu du joueur
+String correctif []= new String[Main.nPions];
+// Nombre de tour de jeu
+int tour = 0;
+int tourRestant = Main.nCoups;
+// Variable true si la partie est gagnée
+boolean boleenSiGagne;
+	
+	RechercheMode1(){
+		
+	if (Main.modeJeu == 1) {
+			//2.1.1 par tableau (combinaison1, reponse1)
+			while (verdict != "GAGNE!" || verdict != "PERDU!") {
+				
+				//2.1.1.1 entrées du joueur (var reponse1)
+				tour++;
+				System.out.println("\n \n Veuillez entrer votre code:");
+				
+				// test de l'entrée
+				
+				TestEntreesJeu entreesJeu = new TestEntreesJeu (Main.reponse1,Main.nCouleurs);
+				
+				//2.1.1.2 comparatif combiaison secrète / entrée => réponse ordinateur
+				boleenSiGagne = true;
+				
+				for (int i=0; i<Main.nPions; i++) {
+					if ( Main.reponse1[i]==Main.combinaisonDefense[i] ) {
+						correctif[i]="=";
+					}
+					if ( Main.reponse1[i]<Main.combinaisonDefense[i] ) {
+						correctif[i]="-";
+						boleenSiGagne = false;
+					}
+					if ( Main.reponse1[i]>Main.combinaisonDefense[i] ) {
+						correctif[i]="+";
+						boleenSiGagne = false;
+					}
+				}
+				//2.1.1.3 Affichage & tests
+				System.out.println("Réponse:");
+				for (int i=0; i<Main.nPions; i++) {
+					System.out.print(correctif[i]);
+				}
+				
+				//2.1.1.4 test
+				tourRestant = Main.nCoups - tour;
+				System.out.println(" -> Il vous reste: "+ tourRestant + " coups!");
+				System.out.println(" TRICHE 2 : reponse1 "+ Main.reponse1 + " 1 combinaisonDefense "+ Main.combinaisonDefense);
+				if (tourRestant == 0 &&  !boleenSiGagne ) {
+					verdict = "PERDU!";
+				}
+				else if (boleenSiGagne) {
+					verdict = "GAGNE!";
+				}
+			}
+			System.out.println(verdict);
+		}
+	}
+}
