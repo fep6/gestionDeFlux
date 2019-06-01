d81d75afb25da1d1d991784355d3f918ef9a4e72
fep <franck.en.planck@gmail.com>
2019-06-01 12:09:23 +0200

    Divers 2 mef -

--- src/packageProjetMasterMind/ComparatifAffichageTest.java
+++ src/packageProjetMasterMind/ComparatifAffichageTest.java
@@ -3,29 +3,31 @@ package packageProjetMasterMind;
 public class ComparatifAffichageTest {
 	
 	//Comparatif combinaison secrète / entrée => réponse ordinateur par "+" ou "-" ou "="			
-	static void comparatifCombinaisonSecrete(int pPion, int pCoup,int pTableauJeu[][],int pCombinaison[], String tableauReponse[][], boolean pboleenSiGagne){
+	static void comparatifCombinaisonSecrete(int pPion, int pCoup,int pTableauJeu[][],int pCombinaison[], String tableauReponse[][], boolean pBoleenSiGagne){
 		for (pPion=0; pPion<Main.nPions; pPion++) {
 			if ( pTableauJeu[pCoup][pPion]==pCombinaison[pPion] ) {
 				tableauReponse[pCoup][pPion]="=";
 			}
 			if ( pTableauJeu[pCoup][pPion]<pCombinaison[pPion] ) {
 				tableauReponse[pCoup][pPion]="+";
-				pboleenSiGagne = false;
+				pBoleenSiGagne = false;
 			}
 			if ( pTableauJeu[pCoup][pPion]>pCombinaison[pPion] ) {
 				tableauReponse[pCoup][pPion]="-";
-				pboleenSiGagne = false;
+				pBoleenSiGagne = false;
 			}
 		}
 	}
 
 	//Affichage & tests
 	static void affichageEtTest(int pPion, int pCoup, String pTableauReponse[][], int pTourRestant, boolean pBoleenSiGagne, String pVerdict){
+		//Affichage réponse
 		System.out.print("preponse: ");
 		for (pPion=0; pPion<Main.nPions; pPion++) {
 			System.out.print(pTableauReponse[pCoup][pPion]);
 		}
-		//2.1.1.4 test
+		System.out.println("");
+		// test
 		if (pTourRestant == 0 && !pBoleenSiGagne ) {
 			pVerdict = "PERDU!";
 		}
